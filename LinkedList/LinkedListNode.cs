fc1d7e4fe9234d461048e55ba2a9128fa75d344f
Jkapura <kapura.yulya@gmail.com>
2016-04-16 20:40:16 +0300

    LinkedListNode<T>

---
+++ LinkedList/LinkedListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    #region LinkedListNode
+    public class LinkedListNode<T>
+    {
+        public T Value { get; internal set; }
+        public LinkedListNode<T> Next { get; internal set; }
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }            
+    }
+    #endregion
+}
